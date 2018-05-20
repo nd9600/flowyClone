70e4d46d31aaa56e5003e9c814dfc46c42cc8f6c
Nathan Douglas <nd9600@gmail.com>
2018-05-20 23:34:51 +0100

    tabbed

--- flowy/src/Home.vue
+++ flowy/src/Home.vue
@@ -30,65 +30,65 @@
 </template>
 
 <script>
-import * as task from "./task.js";
+    import * as task from "./task.js";
 
-export default {
-    name: "home",
-    data() {
-        return {
-            tasks: [],
+    export default {
+        name: "home",
+        data() {
+            return {
+                tasks: [],
+            }
+        },
+        methods: {
+            loadTasks: function() {
+                this.tasks.push(... [
+                    new task.Task("t1", "", "", ["test"]),
+                    new task.Task("t2", "", "", ["test"]),
+                    new task.Task("t3", "", "", ["test3"])
+                ]);
+            }
+        },
+        computed: {
+            getTags() {
+                return this.tasks.map(t => t.tags).flatten();
+            }
+        },
+        created() {
+            this.loadTasks();
         }
-    },
-    methods: {
-        loadTasks: function() {
-            this.tasks.push(... [
-                new task.Task("t1", "", "", ["test"]),
-                new task.Task("t2", "", "", ["test"]),
-                new task.Task("t3", "", "", ["test3"])
-            ]);
-        }
-    },
-    computed: {
-        getTags() {
-            return this.tasks.map(t => t.tags).flatten();
-        }
-    },
-    created() {
-        this.loadTasks();
     }
-}
 </script>
 
 <style>
-#home {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin: 30px 0 0 30px;
-}
+    #home {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        margin: 30px 0 0 30px;
+    }
 
-ul {
-    padding: 0;
-}
+    ul {
+        padding: 0;
+    }
 
-li {
-    margin: 0 10px;
-}
+    li {
+        margin: 0 10px;
+    }
 
-a {
-    color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 
-.tagsContainer {
-    display: inline;
-}
+    .tagsContainer {
+        display: inline;
+    }
 
-.tagLink {
-    color: #999;
-    margin: 0 5px 0 0;
-}
-.tagLink:hover {
-    background-color: #f2f2f2;
-}
+    .tagLink {
+        color: #999;
+        margin: 0 5px 0 0;
+    }
+    .tagLink:hover {
+        background-color: #f2f2f2;
+    }
 </style>
