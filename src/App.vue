b11df49280c0d41dfe49c7475bb2c5332762f9f6
Nathan Douglas <nd9600@gmail.com>
2018-05-25 16:41:40 +0100

    made app the root

--- src/App.vue
+++ src/App.vue
@@ -16,27 +16,70 @@ import Home from "./components/Home.vue";
 import DetailedTask from "./components/DetailedTask.vue";
 import {mapMutations} from "vuex";
 
+import {store, STORAGE_KEY} from "./store/store.js";
+
 export default {
     name: "app",
-    props: ["currentComponent", "componentProp"],
+    el: '#app',
+    data: {
+        currentComponent: "home",
+        componentProp: {tasks: []},
+        tasks: [],
+        shouldUpdateTasks: true
+    },
     components: {
         Home,
         DetailedTask  
     },
     methods: {
         ...mapMutations([
+            "initialiseTaskStorageUID",
             "changeSearchTerm"
-        ])
+        ]),
+        changeCurrentComponent(component, prop) {
+            if (this.currentComponent !== component) {
+                this.currentComponent = component;
+            }
+            this.componentProp = prop;
+        }
+    },
+    watch: {
+        // needs a deep watcher because the array has objects in it
+        tasks: {
+            handler: function (newTasks, oldTasks) { 
+                if (this.shouldUpdateTasks) {
+                    localStorage.setItem(STORAGE_KEY + "-tasks", JSON.stringify(newTasks));
+                }
+            },
+            deep: true
+        }
     },
     created: function() {
+        this.initialiseTaskStorageUID();
+
+        this.shouldUpdateTasks = false;
+        if (localStorage.getItem(STORAGE_KEY + "-tasks") === null) {
+            this.tasks = [];
+        } else {
+            this.tasks = JSON.parse(localStorage.getItem(STORAGE_KEY + "-tasks"));
+        }
+        this.shouldUpdateTasks = true;
+
+        this.changeCurrentComponent("home", {tasks: this.tasks});
+
+        this.$on("change-component-event", (component, prop) => {
+            this.changeCurrentComponent(component, prop);
+        });
+
         //clear the search term when escape is pressed
         //arrow function preserves context
         window.addEventListener('keyup', (event) => {
             if (event.keyCode == 27) {
                 this.changeSearchTerm("");
             }
-      });
-    }
+        });
+    },
+    store
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
-import {mapMutations} from "vuex";
 
 import App from './App.vue';
-
-import {store, STORAGE_KEY} from "./store/store.js";
 
 import Tasks from "./components/Tasks.vue";
 import Tags from "./components/Tags.vue";
@@ -23,60 +20,4 @@ Array.prototype.flatten = function() {
     return [].concat(...this);
 };
 
-new Vue({
-    el: '#app',
-    template: `<app
-        :currentComponent="this.currentComponent"
-        :componentProp="this.componentProp"
-    ></app>`,
-    data: {
-        currentComponent: "home",
-        componentProp: {tasks: []},
-        tasks: [],
-        shouldUpdateTasks: true
-    },
-    components: {
-        App
-    },
-    methods: {
-        ...mapMutations([
-            "initialiseTaskStorageUID"
-        ]),
-        changeCurrentComponent(component, prop) {
-            if (this.currentComponent !== component) {
-                this.currentComponent = component;
-            }
-            this.componentProp = prop;
-        }
-    },
-    watch: {
-        // needs a deep watcher because the array has objects in it
-        tasks: {
-            handler: function (newTasks, oldTasks) { 
-                if (this.shouldUpdateTasks) {
-                    localStorage.setItem(STORAGE_KEY + "-tasks", JSON.stringify(newTasks));
-                }
-            },
-            deep: true
-        }
-    },
-    created() {
-
-        this.initialiseTaskStorageUID();
-
-        this.shouldUpdateTasks = false;
-        if (localStorage.getItem(STORAGE_KEY + "-tasks") === null) {
-            this.tasks = [];
-        } else {
-            this.tasks = JSON.parse(localStorage.getItem(STORAGE_KEY + "-tasks"));
-        }
-        this.shouldUpdateTasks = true;
-
-        this.changeCurrentComponent("home", {tasks: this.tasks});
-
-        this.$on("change-component-event", (component, prop) => {
-            this.changeCurrentComponent(component, prop);
-        });
-    },
-    store
-})
+new Vue(App);
