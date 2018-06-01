a9c99a59c1affd9737f8a4e0dd5afc39c52212f8
Nathan Douglas <nd9600@gmail.com>
2018-06-01 19:19:29 +0100

    fixed vuex

--- src/store/modules/tasksModule.js
+++ src/store/modules/tasksModule.js
@@ -3,59 +3,62 @@ import {getTagsInString} from "../../base/task.js";
 const STORAGE_KEY = 'tasks-flowyClone';
 
 const state = {
-    tasks: new Map(),
+    tasks: new Map(,
     tasksChangeTracker: 1,
     rootTaskIDs: [],
     taskStorageUID: 0
 }
 
 const getters = {
-    tasks(state, gettersArg) {
+    tasks: (state, otherGetters) => {
         return state.tasksChangeTracker && state.tasks;
     },
-    tasksAsArray(state, gettersArg) {
-        return Array.from(gettersArg.tasks.values());
+    tasksAsArray: (state, otherGetters) => {
+        return Object.values(otherGetters.tasks);
     },
-    taskByID: (state, gettersArg) => id => {
-        return gettersArg.tasks.get(id);
+    taskByID: (state, otherGetters) => id => {
+        return otherGetters.tasks[id];
     },
-    hasTask: (state, gettersArg) => id => {
-        return gettersArg.tasks.has(id);
+    hasTask: (state, otherGetters) => id => {
+        return typeof(otherGetters.tasks) !== "undefined";
     },
-    tasksInTask: (state, gettersArg) => id => {
-        if (! gettersArg.hasTask(id)) {
+    tasksInTask: (state, otherGetters) => id => {
+        console.log(otherGetters);
+        if (! getters.hasTask(state)(id)) {
             return [];
         }
-        let thisTask = gettersArg.taskByID(id);
-        return thisTask.tasks.map(taskID => gettersArg.taskByID(taskID));
+        let thisTask = getters.taskByID(state)(id);
+        return thisTask.tasks.map(taskID => getters.taskByID(state, getters)(taskID));
     },
 
-    rootTaskIDs(state, gettersArg) {
+    rootTaskIDs(state, otherGetters) {
         return state.rootTaskIDs;
     },
-    rootTasks(state, gettersArg) {
-        return gettersArg.rootTaskIDs.map(id => gettersArg.taskByID(id));
+    rootTasks(state, otherGetters) {
+        console.log(otherGetters.tasks);
+        console.log(otherGetters.taskByID(String(1)));
+        return otherGetters.rootTaskIDs.map(id => otherGetters.taskByID(state, otherGetters)(String(id)));
     },
 
-    tagsInTask: (state, gettersArg) => task => {
+    tagsInTask: (state, otherGetters) => task => {
         let tagsInContent = getTagsInString(task.content);
         let tagsInDescription = getTagsInString(task.description);
         let tagsToReturn = tagsInContent.concat(tagsInDescription);
         if ((typeof task.tasks !== "undefined") && task.tasks.length > 0) {
-            let innerTasks = gettersArg.tagsInTasks(task.tasks)
+            let innerTasks = getters.tagsInTasks(state)(task.tasks)
             tagsToReturn = tagsToReturn.concat(innerTasks);
         }
         return tagsToReturn.unique();
     },
 
-    tagsInTasks: (state, gettersArg) => (tasks) => {
+    tagsInTasks: (state, otherGetters) => (tasks) => {
         return tasks.map(taskID => {
-            let innerTask = gettersArg.taskByID(taskID);
-            return gettersArg.tagsInTask(innerTask);
+            let innerTask = getters.taskByID(state, getters)(taskID);
+            return getters.tagsInTask(state)(innerTask);
         }).flatten().unique();
     },
 
-    taskStorageUID(state, gettersArg) {
+    taskStorageUID(state) {
         return state.taskStorageUID;
     }
 }
@@ -63,6 +66,9 @@ const getters = {
 const mutations = {
     incrementTaskChangeTracker(state) {
         state.tasksChangeTracker += 1;
+
+        let localTasksKey = STORAGE_KEY + "-tasks";
+        //localStorage.setItem(localTasksKey, JSON.stringify(getters.tasks(state)));
     },
     setTasks(state, tasks) {
         state.tasks = JSON.parse(JSON.stringify(tasks));
@@ -70,6 +76,9 @@ const mutations = {
     },
     setTask(state, task) {
         let taskID = task["id"];
+        console.log("setTask");
+        console.trace();
+        console.log(state.tasks);
         state.tasks.set(taskID, task);
         mutations.incrementTaskChangeTracker(state);
     },
@@ -88,7 +97,7 @@ const mutations = {
     // need to add mutation for removing an inner task
 
     addTaskToTask(state, {taskID, newTaskID}) {
-        let task = state.tasks.get(taskID);
+        let task = getters.taskByID(state)(taskID);
         task.tasks.push(newTaskID);
         mutations.incrementTaskChangeTracker(state);
     },
@@ -98,24 +107,16 @@ const mutations = {
     },
 
     initialiseTasks(state) {
-        let tasksKey = STORAGE_KEY + "-tasks";
-        let mapStringFromLocalStorage = localStorage.getItem(tasksKey);
-        if (mapStringFromLocalStorage) {
-            state.tasks = new Map(JSON.parse(mapStringFromLocalStorage));
-        }
+        console.log("initialiseTasks");
+        console.log("localStorage.getItem(STORAGE_KEY + '-tasks')");
+        // if (localStorage.getItem(STORAGE_KEY + "-tasks")) {
+        //     mutations.incrementTaskChangeTracker(state);
+        //     state.tasks = JSON.parse(localStorage.getItem(STORAGE_KEY + "-tasks"));
+        // }
 
-        let rootTaskIDsKey = STORAGE_KEY + "-rootTaskIDs";
-        let rootTaskIDsString = localStorage.getItem(rootTaskIDsKey);
-        if (rootTaskIDsString) {
-            state.rootTaskIDs = JSON.parse(rootTaskIDsString);
-        }
-
-        let UIDKey = STORAGE_KEY + "-taskStorageUID";
-        let UIDString = localStorage.getItem(UIDKey);
-        if (UIDString) {
-            state.taskStorageUID = JSON.parse(UIDString);
-        }
-        mutations.incrementTaskChangeTracker(state);
+        // if (localStorage.getItem(STORAGE_KEY + "-taskStorageUID")) {
+        //     state.taskStorageUID = JSON.parse(localStorage.getItem(STORAGE_KEY + "-taskStorageUID"));
+        // }
     },
     incrementTaskStorageUID: (state) => {
         state.taskStorageUID++;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import tasks from "./modules/tasks.js";
-import searchTerm from "./modules/searchTerm.js";
+import tasksModule from "./modules/tasks.js";
+import searchTermModule from "./modules/searchTerm.js";
 
 Vue.use(Vuex);
 
@@ -13,18 +13,22 @@ export {STORAGE_KEY, store};
 const store = new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',    
     modules: {
-        tasks,
-        searchTerm
+        tasksModule,
+        searchTermModule
     }
 });
 
 store.subscribe((mutation, state) => {
-    // console.log(mutation);
     if (["setTask", "removeTask", "addTaskToTask", "addTaskToRoot"].indexOf(mutation.type) > -1) {
-        // let localTasksKey = STORAGE_KEY + "-tasks";
-        // localStorage.setItem(localTasksKey, JSON.stringify(state.tasks.tasks));
+        let localTasksKey = STORAGE_KEY + "-tasks";
+        let mapString = JSON.stringify(Array.from(state.tasksModule.tasks.entries()));
+        localStorage.setItem(localTasksKey, mapString);
+
+        let rootTaskIDsKey = STORAGE_KEY + "-rootTaskIDs";
+        let rootTaskIDsString = JSON.stringify(state.tasksModule.rootTaskIDs);
+        localStorage.setItem(rootTaskIDsKey, rootTaskIDsString);
 
         let localUIDkey = STORAGE_KEY + "-taskStorageUID";
-        localStorage.setItem(localUIDkey, JSON.stringify(state.tasks.taskStorageUID));
+        localStorage.setItem(localUIDkey, JSON.stringify(state.tasksModule.taskStorageUID));
     }
 });
