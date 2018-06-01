da2bf39089f72cb4c70628446fce637b2761b25f
Nathan Douglas <nd9600@gmail.com>
2018-06-01 21:26:16 +0100

    debounced local storage saving

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,8 @@ import Vuex from "vuex";
 import tasksModule from "./modules/tasks.js";
 import searchTermModule from "./modules/searchTerm.js";
 
+import debounce from "lodash.debounce";
+
 Vue.use(Vuex);
 
 const STORAGE_KEY = 'tasks-flowyClone';
@@ -18,17 +20,22 @@ const store = new Vuex.Store({
     }
 });
 
-store.subscribe((mutation, state) => {
-    if (["setTask", "removeTask", "addTaskToTask", "addTaskToRoot"].indexOf(mutation.type) > -1) {
-        let localTasksKey = STORAGE_KEY + "-tasks";
-        let mapString = JSON.stringify(Array.from(state.tasksModule.tasks.entries()));
-        localStorage.setItem(localTasksKey, mapString);
+store.subscribe(
+    debounce((mutation, state) => {
+        if (["setTask", "removeTask", "addTaskToTask", "addTaskToRoot"].indexOf(mutation.type) > -1) {
+            let localTasksKey = STORAGE_KEY + "-tasks";
+            let mapString = JSON.stringify(Array.from(state.tasksModule.tasks.entries()));
+            localStorage.setItem(localTasksKey, mapString);
 
-        let rootTaskIDsKey = STORAGE_KEY + "-rootTaskIDs";
-        let rootTaskIDsString = JSON.stringify(state.tasksModule.rootTaskIDs);
-        localStorage.setItem(rootTaskIDsKey, rootTaskIDsString);
+            let rootTaskIDsKey = STORAGE_KEY + "-rootTaskIDs";
+            let rootTaskIDsString = JSON.stringify(state.tasksModule.rootTaskIDs);
+            localStorage.setItem(rootTaskIDsKey, rootTaskIDsString);
 
-        let localUIDkey = STORAGE_KEY + "-taskStorageUID";
-        localStorage.setItem(localUIDkey, JSON.stringify(state.tasksModule.taskStorageUID));
-    }
-});
+            let localUIDkey = STORAGE_KEY + "-taskStorageUID";
+            localStorage.setItem(
+                localUIDkey,
+                JSON.stringify(state.tasksModule.taskStorageUID)
+            );
+        }
+    }, 500)
+);
