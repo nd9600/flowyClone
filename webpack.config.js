b2e21e4a64920da18e1ad279bcafc5b8116f3a77
Nathan Douglas <nd9600@gmail.com>
2018-09-05 14:19:04 +0100

    colon

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     mode: process.env.NODE_ENV,
